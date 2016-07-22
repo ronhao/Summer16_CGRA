d14d57417f7ded845246fe810296397b19f5b7f2
OrangeLover <yh326@cornell.edu>
2016-07-22 11:28:57 -0400

    vv-add working

--- tests/mem_test.c
+++ tests/mem_test.c
@@ -12,22 +12,20 @@ int main() {
   do {
     // Setup some test data
     unsigned int x_in = 715;
-    unsigned long long y_in = 0;
+    //unsigned long long y_in = 0;
     unsigned int result = 120;
     
     asm volatile ("fence");
     // Invoke the acclerator and check responses
     // test 1:
     //              opcode rd rs1      rs2 funct   
-    asm volatile ("custom0 %0, %[x], %[y], 0"  :  "=r"(result) : [x]"r"(x_in), [y]"r"(y_in));
-    printf("test 1: stored data %d into address %d, mem_resp_store_data is %d\n", x_in, y_in, result);
+    asm volatile ("custom0 %0, %[x], %[y], 0"  :  "=r"(result) : [x]"r"(x_in), [y]"r"(&x_in));
+    printf("test 1: stored data %d into address %d, mem_resp_store_data is %d\n", x_in, &x_in, result);
     
     asm volatile ("fence");
     // test 2:
-    x_in = 0;
-    y_in = 0;
-    asm volatile ("custom0 %0, %[x], %[y], 1"  :  "=r"(result) : [x]"r"(x_in), [y]"r"(y_in));
-    printf("test 2: data loaded from address %d is %d\n", y_in, result);
+    asm volatile ("custom0 %0, %[x], %[y], 1"  :  "=r"(result) : [x]"r"(x_in), [y]"r"(&x_in));
+    printf("test 2: data loaded from address %d is %d\n", &x_in, result);
     
     asm volatile ("fence");
   }while(0);
