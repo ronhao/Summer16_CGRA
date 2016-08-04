f8a32b3729fedc817fe44574b799aafaec731ace
OrangeLover <yh326@cornell.edu>
2016-08-03 23:14:12 -0400

    with digitrec but not working yet

--- vsim/accel_src/mem_Accel_B.v
+++ vsim/accel_src/mem_Accel_B.v
@@ -587,21 +587,4 @@ module mem_Accel_B_Ctrl
   end
 
 endmodule
-
-module vc_Adder
-#(
-  parameter p_nbits = 1
-)(
-  in0,
-  in1,
-  out
-);
-
-  input  [p_nbits-1:0] in0;
-  input  [p_nbits-1:0] in1;
-  output [p_nbits-1:0] out;
-
-  assign out = in0 + in1;
-
-endmodule
   
