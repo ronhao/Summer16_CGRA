b21d1dad88b13b6e9ce9f47752b76172a82bc345
OrangeLover <yh326@cornell.edu>
2016-08-05 15:23:15 -0400

    0805 commit

--- tests/add_test.c
+++ tests/add_test.c
@@ -13,13 +13,14 @@ int main() {
     // Setup some test data
     unsigned int x_in = 715;
     unsigned int y_in = 721;
-    unsigned int result = 120;
+    unsigned int result;
     
+    printf("add\n");
     asm volatile ("fence");
     // Invoke the acclerator and check responses
     // test 1:
-    //              opcode rd rs1      rs2 funct   
-    asm volatile ("custom0 %0, %[x], %[y], 2"  :  "=r"(result) : [x]"r"(x_in), [y]"r"(y_in));
+    //              funct rd rs1      rs2 opcode   
+    asm volatile ("custom0 %0, %[x], %[y], 1"  :  "=r"(result) : [x]"r"(x_in), [y]"r"(y_in));
     printf("test 1: add %d to %d, the result is %d\n", x_in, y_in, result);
     
   }while(0);
